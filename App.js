febc9bf2509ff588e45d80b401d4507e506a9825
richardikang <76178368+richardikang@users.noreply.github.com>
2021-08-03 16:15:06 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WarGame from './WarGame.js';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App-body">
+    <WarGame> </WarGame>
+    </div>
+  </div>
+);
+}
+export default App;
